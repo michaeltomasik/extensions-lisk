899083978b7e29e3b8e6ebd1ea472a23c0f0d88e
michaeltomasik <shugo12345@gmail.com>
2019-02-26 11:07:48 +0100

    first commit

---
+++ latestBlock.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import LiskHubExtensions from 'LiskHubExtensions'
+
+class Hello extends React.Component {
+  render () {
+    return <LiskHubExtensions.components.Box>
+      <h1>{this.props.t('Last Blocks')}</h1> 
+      {this.props.data.latestBlocks ? this.props.data.latestBlocks.map((block, index) =>
+      <div>{index}. {block.id}</div>) : "NO BLOCKS"}
+    </LiskHubExtensions.components.Box>
+  }
+}
+
+export default Hello
+
+
+LiskHubExtensions.addModule({
+  identifier:
+  LiskHubExtensions.identifiers.dashboardColumn2,
+  component: Hello
+});
---
+++ timeComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import LiskHubExtensions from 'LiskHubExtensions'
+
+class Hello extends React.Component {
+  render () {
+    return <LiskHubExtensions.components.Box><h1>{this.props.t('Last  click at ')} {this.props.data.time}</h1> <LiskHubExtensions.components.Button
+    label='Check time'
+    onClick={() => {this.props.onClickHandle();}} /></LiskHubExtensions.components.Box>
+  }
+}
+
+export default Hello
+
+LiskHubExtensions.addModule({
+  identifier:
+  LiskHubExtensions.identifiers.dashboardColumn1,
+  component: Hello
+});
