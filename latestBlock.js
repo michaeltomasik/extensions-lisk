17d55d22bfb3d7f5da35e01a429ac2e0781c73fb
Michał Tomasik <shugo12345@gmail.com>
2019-03-22 11:38:22 +0100

    Update latestBlock.js

--- latestBlock.js
+++ latestBlock.js
@@ -4,7 +4,9 @@ import LiskHubExtensions from 'LiskHubExtensions'
 class Hello extends React.Component {
   render () {
     return <LiskHubExtensions.components.Box>
-      <header>{this.props.t('Last Blocks')}</header> 
+      <header>
+        <h1>{this.props.t('Last Blocks')}</h1>
+      </header> 
       {this.props.data.latestBlocks ? this.props.data.latestBlocks.map((block, index) =>
       <div>{index}. {block.id}</div>) : "NO BLOCKS"}
     </LiskHubExtensions.components.Box>
